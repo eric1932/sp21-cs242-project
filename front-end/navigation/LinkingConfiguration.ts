e50539aa31201ee0ae794c687efa57dc08dc3ee5
Eric <eric1932777@gmail.com>
2021-04-19 15:07:27 +0800

    Add LoginScreen and set as the default one

--- front-end/navigation/LinkingConfiguration.ts
+++ front-end/navigation/LinkingConfiguration.ts
@@ -9,22 +9,29 @@ import * as Linking from 'expo-linking';
 export default {
   prefixes: [Linking.makeUrl('/')],
   config: {
+    // screens: {
+    //   Root: {
+    //     screens: {
+    //       TabOne: {
+    //         screens: {
+    //           TabOneScreen: 'one',
+    //         },
+    //       },
+    //       TabTwo: {
+    //         screens: {
+    //           TabTwoScreen: 'two',
+    //         },
+    //       },
+    //     },
+    //   },
+    //   NotFound: '*',
+    // },
     screens: {
-      Root: {
+      Login: {
         screens: {
-          TabOne: {
-            screens: {
-              TabOneScreen: 'one',
-            },
-          },
-          TabTwo: {
-            screens: {
-              TabTwoScreen: 'two',
-            },
-          },
-        },
-      },
-      NotFound: '*',
-    },
+          LoginScreen: 'login'
+        }
+      }
+    }
   },
 };
--- front-end/navigation/index.tsx
+++ front-end/navigation/index.tsx
@@ -3,15 +3,16 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {DarkTheme, DefaultTheme, NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import * as React from 'react';
-import { ColorSchemeName } from 'react-native';
+import {ColorSchemeName} from 'react-native';
 
 import NotFoundScreen from '../screens/NotFoundScreen';
-import { RootStackParamList } from '../types';
+import {RootStackParamList} from '../types';
 import BottomTabNavigator from './BottomTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
+import LoginScreen from "../screens/LoginScreen";
 
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
   return (
@@ -30,6 +31,7 @@ const Stack = createStackNavigator<RootStackParamList>();
 function RootNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Root" component={BottomTabNavigator} />
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
     </Stack.Navigator>
